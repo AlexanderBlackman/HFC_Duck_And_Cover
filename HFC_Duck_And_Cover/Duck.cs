08fc917e6bbd471fcbdfb2bc6eca45889913b3bf
Alex Blackman <alexander.blackman@gmail.com>
2021-01-31 07:07:27 +1100

    Add project files.

---
+++ HFC_Duck_And_Cover/Duck.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace HFC_Duck_And_Cover
+{
+    class Duck//: IComparable<Duck>
+    {
+        public int Size { get; set; }
+        public KindOfDuck Kind { get; set; }
+
+    /*    public int CompareTo(Duck duckToCompare)
+        {
+            if (this.Size > duckToCompare.Size)
+                return 1;
+            else if (this.Size < duckToCompare.Size)
+                return -1;
+            else
+                return 0;
+        }*/
+
+        public enum KindOfDuck
+        {
+            Mallard,
+            Muscovy,
+            Loon,
+            Peking
+        }
+
+ 
+    }
+
+    enum SortCriteria
+    {
+        SizeThenKind,
+        KindThenSize,
+    }
+
+    class DuckComparer: IComparer<Duck>
+    {
+        public SortCriteria SortBy = SortCriteria.SizeThenKind;
+
+    public int Compare(Duck x, Duck y)
+        {
+            if (SortBy == SortCriteria.SizeThenKind)
+            {
+                if (x.Size > y.Size)
+                    return 1;
+                else if (x.Size < y.Size)
+                    return -1;
+                else
+                    return 0;
+            }
+            else if (SortBy == SortCriteria.KindThenSize)
+            {
+                if (x.Kind > y.Kind)
+                    return 1;
+                else if (x.Kind < y.Kind)
+                    return -1;
+                else
+                    return 0;
+            }
+            //This superfulous else statement allows us to add new things to compare in the future.
+            else 
+                return 0;
+        }
+    }
+}
---
+++ HFC_Duck_And_Cover/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HFC_Duck_And_Cover
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+
+
+            DuckComparer comparer = new DuckComparer();
+            List<Duck> myFlock = new List<Duck>()
+            {
+                new Duck(){Size=12, Kind= Duck.KindOfDuck.Loon},
+                new Duck(){Size=15, Kind = Duck.KindOfDuck.Peking},
+                new Duck(){Size=19, Kind = Duck.KindOfDuck.Muscovy},
+                new Duck(){Size = 10, Kind = Duck.KindOfDuck.Mallard},
+                new Duck(){Size = 14, Kind = Duck.KindOfDuck.Muscovy},
+                new Duck(){Size = 15, Kind = Duck.KindOfDuck.Loon}
+            };
+
+
+
+            Console.WriteLine("\nSorting by kind then size\n");
+            comparer.SortBy = SortCriteria.KindThenSize;
+            myFlock.Sort(comparer);
+            PrintDucks(myFlock);
+            Console.WriteLine("\nSorting by size, then kind\n" );
+            comparer.SortBy = SortCriteria.SizeThenKind;
+            myFlock.Sort(comparer);
+            PrintDucks(myFlock);
+
+        }
+        
+        public static void PrintDucks(List<Duck> ducks)
+        {
+            foreach (Duck duck in ducks)
+            {
+                Console.WriteLine($"A {duck.Size}\" {duck.Kind}");
+            }
+        }
+        
+    }
+}
